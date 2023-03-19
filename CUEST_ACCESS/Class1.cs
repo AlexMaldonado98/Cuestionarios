1a3218e0b87b6e9c5659c24c0418731f2e9cf65c
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2023-03-18 21:26:21 -0600

    Agregar archivos de proyecto.

---
+++ CUEST_ACCESS/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUEST_ACCESS
+{
+    public class Class1
+    {
+        cuestionariosDBEntities db = new cuestionariosDBEntities();
+
+        public List<cuestionarios> Listar()
+        {
+            var cuestionarios = from a in db.cuestionarios
+                                select a;
+            return cuestionarios.ToList();
+        }
+        public void Agregar(cuestionarios item)
+        {
+            db.cuestionarios.Add(item);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Cuestionarios/Controllers/CuestionarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cuestionarios.Controllers
+{
+    public class CuestionarioController : Controller
+    {
+        CUEST_ACCESS.Class1 a = new CUEST_ACCESS.Class1();
+
+        // GET: Cuestionario
+        public ActionResult Index()
+        {
+            var cuestionarios = a.Listar();
+            return View(cuestionarios);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new CUEST_ACCESS.cuestionarios());
+        }
+        [HttpPost]
+
+        public ActionResult Create(CUEST_ACCESS.cuestionarios item)
+        {
+            a.Agregar(item);
+            return View("index", a.Listar());
+        }
+
+
+    }
+}
